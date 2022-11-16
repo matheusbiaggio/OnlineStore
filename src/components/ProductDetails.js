ffe8e24d4b28ee43b9a5720faeb99139f7e7829d
Matheus Biaggio <matheusbiaggio@gmail.br>
2022-11-16 16:29:10 -0300

    termino req09

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -7,6 +7,7 @@ import { getProductById } from '../services/api';
 class ProductDetails extends React.Component {
   state = {
     product: [],
+    shopCart: [],
   };
 
   async componentDidMount() {
@@ -14,20 +15,64 @@ class ProductDetails extends React.Component {
     const { id } = match.params;
     const details = await getProductById(id);
     this.setState({ product: details });
+    this.getLocalStorage();
   }
+
+  // Pega os valores do localStorage
+  getLocalStorage = () => {
+    const shopCartLS = JSON.parse(localStorage.getItem('shopCart'));
+    if (shopCartLS !== null) {
+      this.setState((prevState) => ({
+        shopCart: [].concat(...prevState.shopCart, shopCartLS),
+      }));
+    }
+  };
+
+  // Adiciona no localStorage
+  handleUpdateLocalStorage = () => {
+    const { shopCart } = this.state;
+    localStorage.setItem('shopCart', JSON.stringify(shopCart));
+  };
+
+  // Busca se ja existe um id no stado
+  filterShopCard = (id) => {
+    const { shopCart } = this.state;
+    const newShopCart = [...shopCart];
+    const findId = newShopCart
+      .find((product) => product.shopCartId === id);
+    const index = newShopCart.findIndex((productIndex) => productIndex.shopCartId === id);
+    if (findId === undefined) {
+      return false;
+    }
+    return index;
+  };
+
+  // Adiciona ao carrinho
+  addShopCart = () => {
+    const { product, shopCart } = this.state;
+    const idElement = this.filterShopCard(product.id);
+    if (idElement === false) {
+      this.setState((prevState) => ({
+        shopCart: [].concat(...prevState.shopCart, {
+          shopCartQuantity: 1,
+          shopCartPrice: product.price,
+          shopCartId: product.id,
+          shopCartTitle: product.title,
+        }),
+      }), this.handleUpdateLocalStorage);
+    } else {
+      shopCart[idElement].shopCartQuantity += 1;
+      this.setState({
+        shopCart,
+      }, this.handleUpdateLocalStorage);
+    }
+  };
 
   render() {
     const { product } = this.state;
     return (
-      <>
-        <p data-testid="product-detail-name">{product.title}</p>
-        <p data-testid="product-detail-price">{product.price}</p>
-        <img
-          src={ product.thumbnail }
-          alt={ product.title }
-          data-testid="product-detail-image"
-        />
-        <Link to="/shoppingCart">
+      <div>
+        <Link to="/cart">
           <button
             type="button"
             data-testid="shopping-cart-button"
@@ -35,8 +80,24 @@ class ProductDetails extends React.Component {
             Ir ao carrinho!!
           </button>
         </Link>
-        <ProductReview productId={ product.id } />
-      </>
+        <div className="product">
+          <p data-testid="product-detail-name">{product.title}</p>
+          <p data-testid="product-detail-price">{product.price}</p>
+          <img
+            src={ product.thumbnail }
+            alt={ product.title }
+            data-testid="product-detail-image"
+          />
+          <button
+            type="button"
+            data-testid="product-detail-add-to-cart"
+            onClick={ this.addShopCart }
+          >
+            Adicionar ao Carrinho
+          </button>
+          <ProductReview productId={ product.id } />
+        </div>
+      </div>
 
     );
   }
